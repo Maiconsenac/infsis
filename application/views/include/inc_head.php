55ba4df0b125cd10493695ef8e03cd9495eb2dc1
Yuri <yuriinternacional86@gmail.com>
2016-10-23 11:15:45 -0200

    Foi mudado o visual da página de login e inserido um icone.

--- application/views/include/inc_head.php
+++ application/views/include/inc_head.php
@@ -1,9 +1,12 @@
 <title>Infsis</title>
 <meta charset="utf-8">
+<!--inseri o imagem de logo na barra do navegador-->
+<link rel="icon" href="<?= base_url('icon/logoInfsis.png') ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?= base_url('bootstrap/css/bootstrap.min.css') ?>">
 <script src="<?= base_url('bootstrap/js/jquery/1.12.4/jquery.min.js') ?>"></script>
 <script src="<?= base_url('bootstrap/js/bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('js/jquery.min.js') ?>"></script>
 <script type="text/javascript" src="<?= base_url('bootstrap/jquery.min.js') ?>"></script>
 <script type="text/javascript" src="<?= base_url('bootstrap/jquery.maskedinput.min.js') ?>"></script>
 <script type="text/javascript" src="<?= base_url('bootstrap/jquery.zoom.js') ?>"></script>
--- application/views/login.php
+++ application/views/login.php
@@ -1,68 +1,128 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title> InfSiS </title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="<?= base_url('bootstrap/css/bootstrap.min.css') ?>">
-        <script src="<?= base_url('bootstrap/js/jquery/1.12.4/jquery.min.js') ?>"></script>
-        <script src="<?= base_url('bootstrap/js/bootstrap.min.js') ?>"></script>
+        <?php include 'include/inc_head.php'; ?>
+        <link href="<?= base_url('assets/css/signin.css') ?>" rel="stylesheet">
     </head>
 
     <style>
-        body {
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #eee;
+        .form-style-6{
+            font: 95% Arial, Helvetica, sans-serif;
+            max-width: 400px;
+            margin: 0px auto;
+            padding: 0px;
+            background: #F7F7F7;
         }
-
-        .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
+        .form-style-6 #titulo{
+            background: #43D1AF;
+            padding: 20px 0;
+            font-size: 140%;
+            font-weight: 300;
+            width: 100%;
+            text-align: center;
+            color: #fff;
+            margin: -5px 0px 0px 0px;
         }
-
-        .form-signin .form-control {
-            position: relative;
-            height: auto;
+        .form-style-6 input[type="text"],
+        .form-style-6 input[type="date"],
+        .form-style-6 input[type="datetime"],
+        .form-style-6 input[type="email"],
+        .form-style-6 input[type="number"],
+        .form-style-6 input[type="search"],
+        .form-style-6 input[type="password"],
+        .form-style-6 input[type="time"],
+        .form-style-6 input[type="url"],
+        .form-style-6 textarea,
+        .form-style-6 select 
+        {
+            -webkit-transition: all 0.30s ease-in-out;
+            -moz-transition: all 0.30s ease-in-out;
+            -ms-transition: all 0.30s ease-in-out;
+            -o-transition: all 0.30s ease-in-out;
+            outline: none;
+            box-sizing: border-box;
             -webkit-box-sizing: border-box;
             -moz-box-sizing: border-box;
+            width: 100%;
+            background: #fff;
+            margin-bottom: 4%;
+            border: 1px solid #ccc;
+            padding: 3%;
+            color: #555;
+            font: 95% Arial, Helvetica, sans-serif;
+        }
+        .form-style-6 input[type="text"]:focus,
+        .form-style-6 input[type="date"]:focus,
+        .form-style-6 input[type="datetime"]:focus,
+        .form-style-6 input[type="email"]:focus,
+        .form-style-6 input[type="number"]:focus,
+        .form-style-6 input[type="search"]:focus,
+        .form-style-6 input[type="time"]:focus,
+        .form-style-6 input[type="url"]:focus,
+        .form-style-6 textarea:focus,
+        .form-style-6 select:focus
+        {
+            box-shadow: 0 0 5px #43D1AF;
+            padding: 3%;
+            border: 1px solid #43D1AF;
+        }
+
+        .form-style-6 input[type="submit"],
+        .form-style-6 input[type="button"]{
             box-sizing: border-box;
-            padding: 10px;
-            font-size: 16px;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            width: 100%;
+            padding: 3%;
+            background: #43D1AF;
+            border-bottom: 2px solid #30C29E;
+            border-top-style: none;
+            border-right-style: none;
+            border-left-style: none;    
+            color: #fff;
+        }
+        .form-style-6 input[type="submit"]:hover,
+        .form-style-6 input[type="button"]:hover{
+            background: #2EBC99;
         }
 
-        .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
+        .form-style-6 #cadastrar-se {
+            -moz-box-shadow: inset 0px 1px 0px 0px #45D6D6;
+            -webkit-box-shadow: inset 0px 1px 0px 0px #45D6D6;
+            box-shadow: inset 0px 1px 0px 0px #45D6D6;
+            background-color: #2CBBBB;
+            border: 1px solid #27A0A0;
+            display: inline-block;
+            cursor: pointer;
+            color: #FFFFFF;
+            font-family: 'Open Sans Condensed', sans-serif;
+            font-size: 14px;
+            padding: 8px 18px;
+            text-decoration: none;
+            text-transform: uppercase;
         }
 
-        .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-        }
-
-        .form-signin-heading{
-            text-align: center;
+        .form-style-6 #cadastrar-se:hover {
+            background:linear-gradient(to bottom, #34CACA 5%, #30C9C9 100%);
+            background-color:#34CACA;
         }
     </style>
 
     <body>
 
         <div class="container">
-
-            <form class="form-signin" method="post" action="<?= base_url('usuarios/logar') ?>">
-                <h2 class="form-signin-heading">Login</h2>
-                <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email" required autofocus>
-                <br />
-                <input type="password" id="inputPassword" class="form-control" name="senha" placeholder="Senha" required>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-               
-            </form>
-
-        </div> 
+            <div class="form-style-6">
+                <input type="button" id="titulo" onclick="document.location.href = '<?= base_url() ?>'" value="Login"/>
+                <form class="form-signin" method="post" action="<?= base_url('usuarios/logar') ?>">
+                    <input type="email" name="email" placeholder="E-mail de Acesso" required autofocus/>
+                    <input type="password" name="senha" placeholder="Senha" required/>
+                    <p>
+                        <input type="submit" value="Entrar" />
+                    </p>
+                    <input id="cadastrar-se" type="button" value="Cadastrar-se" />
+                </form>
+            </div>
+        </div>
 
     </body>
 </html>
